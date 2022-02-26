e8f4cf54f0dbba7f755585836577770317974265
kolism <14796164+kolism@users.noreply.github.com>
2022-02-26 14:04:55 -0500

    Ninja in, custom element issue. to try loading as script

--- src/pages/content/index.js
+++ src/pages/content/index.js
@@ -1,61 +1,87 @@
-import React, { Component } from "react";
+import { useState, useRef, useEffect } from "react";
+import React from "react";
 import { render } from "react-dom";
 import "./index.css";
 import { Store } from "webext-redux";
 import { Provider } from "react-redux";
 import { connect } from "react-redux";
+import "ninja-keys";
+import "@material/mwc-icon";
 
 const store = new Store({
   portName: "COUNTING",
 });
 
-export default class InjectApp extends Component {
-  constructor(props) {
-    super(props);
-    this.classButtonDetail = "";
-    this.classPopupDetail = "";
-    console.log("PROPS", props);
-  }
+const overlayStyle = {
+  zIndex: 99999999,
+};
 
-  render() {
-    if (!this.props.settings || !this.props.animation)
-      return <h1> funky props :)(</h1>;
-    if (this.props.animation.buttonCog) {
-      this.classButtonDetail = "circle faa-tada ";
-      this.classPopupDetail = "popup popup-visibility-show ";
-      setTimeout(function () {
-        store.dispatch({ type: "TOGGLE-COG", buttonCog: false });
-      }, 3500);
+const iconMsg = `<svg xmlns="http://www.w3.org/2000/svg" class="ninja-icon" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z" />
+</svg>`;
+
+export default function InjectApp(props) {
+  const ninjaKeys = useRef(null);
+  const [hotkeys, setHotkeys] = useState([
+    {
+      id: "Home",
+      title: "Open Home",
+      hotkey: "ctrl+H",
+      icon: iconMsg,
+      handler: () => {
+        console.log("navigation to home");
+      },
+    },
+    {
+      id: "Open Projects",
+      title: "Open Projects",
+      hotkey: "ctrl+T",
+      icon: iconMsg,
+      handler: () => {
+        console.log("navigation to projects");
+      },
+    },
+    {
+      id: "Theme",
+      title: "Change theme...",
+      icon: iconMsg,
+      children: [
+        {
+          id: "Light Theme",
+          title: "Change theme to Light",
+          icon: iconMsg,
+          handler: () => {
+            console.log("theme light");
+          },
+        },
+        {
+          id: "Dark Theme",
+          title: "Change theme to Dark",
+          icon: iconMsg,
+          keywords: "lol",
+          handler: () => {
+            console.log("theme dark");
+          },
+        },
+      ],
+    },
+  ]);
+
+  useEffect(() => {
+    if (ninjaKeys.current) {
+      ninjaKeys.current.data = hotkeys;
     }
-    if (!this.props.animation.buttonCog) {
-      this.classButtonDetail = "circle ";
-      this.classPopupDetail = "popup popup-visibility-hide";
-    }
-    return (
-      <div>
-        HELLLO! {this.props.currentTab}
-        {this.props.actions}
-        {this.props.settings.actions}
-        {this.props.tabs.allTabs.length}
-        <div
-          className={
-            this.props.settings.button
-              ? `${this.classButtonDetail} visible`
-              : `${this.classButtonDetail} hidden`
-          }
-          accessKey="s"
-          onClick={() => {
-            store.dispatch({ type: "ADD-FROM-BUTTON", addFromButton: true });
-            store.dispatch({ type: "TOGGLE-COG", buttonCog: true });
-          }}
-        >
-          <div className={this.classPopupDetail}>Saved</div>
-          <div className="x-line"></div>
-          <div className="y-line"></div>
-        </div>
-      </div>
-    );
-  }
+  }, []);
+  return (
+    <div>
+      <ninja-keys ref={ninjaKeys} style={overlayStyle}></ninja-keys>
+      HELLLO! {props.currentTab}
+      {props.actions}
+      {props.settings.actions}
+      {props.tabs.allTabs.length}
+    </div>
+  );
+  /* } */
 }
 
 const mapStateToProps = (state) => {
