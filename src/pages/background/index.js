c82d8436579c663a111e74f5985d9e31d4e9fd03
Marc K <14796164+kolism@users.noreply.github.com>
2022-02-26 23:39:48 -0500

    Update index.js

--- src/pages/background/index.js
+++ src/pages/background/index.js
@@ -5,23 +5,9 @@ import { applyMiddleware, createStore } from "redux";
 import reducer from "./reducers";
 
 import { saveState, loadState } from "./localStorage";
-let actions = [];
 
-const getTabs = () => {
-  chrome.tabs.query({}, (tabs) => {
-    tabs.forEach((tab) => {
-      tab.desc = "Chrome tab";
-      tab.keycheck = false;
-      tab.action = "switch-tab";
-      tab.type = "tab";
-    });
-    actions = tabs.concat(actions);
-  });
-};
-console.log("actions", actions);
 const store = createStore(reducer, loadState());
 
-store.dispatch({ type: "TAB-ACTIONS", actions: actions });
 wrapStore(store, { portName: "COUNTING" }); // make sure portName matches
 
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => updateAlphaBar());
